9abdfaaee9af091978cc14d60206137815f7b767
Avi Putri Pertiwi <aviputripertiwi@Bernstein-Rundkopf.local>
2018-12-23 20:19:23 +0100

    -a

---
+++ example_FeatureSelection_RF.py
@@ -0,0 +1,77 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import accuracy_score
+
+### Create The Data
+# Load the iris dataset
+iris = datasets.load_iris()
+
+# Create a list of feature names
+feat_labels = ['Sepal Length','Sepal Width','Petal Length','Petal Width']
+
+# Create X from the features
+X = iris.data
+
+# Create y from output
+y = iris.target
+
+### Split The Data Into Training And Test Sets
+# Split the data into 40% test and 60% training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+
+
+### Train A Random Forest Classifier
+# Create a random forest classifier
+clf = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Print the name and gini importance of each feature
+for feature in zip(feat_labels, clf.feature_importances_):
+    print(feature)
+
+### Identify And Select Most Important Features
+
+# Create a selector object that will use the random forest classifier to identify
+# features that have an importance of more than 0.15
+sfm = SelectFromModel(clf, threshold=0.15)
+
+# Train the selector
+sfm.fit(X_train, y_train)
+
+# Print the names of the most important features
+for feature_list_index in sfm.get_support(indices=True):
+    print(feat_labels[feature_list_index])
+
+
+### Create A Data Subset With Only The Most Important Features
+# Transform the data to create a new dataset containing only the most important features
+# Note: We have to apply the transform to both the training X and test X data.
+X_important_train = sfm.transform(X_train)
+X_important_test = sfm.transform(X_test)
+
+
+### Train A New Random Forest Classifier Using Only Most Important Features
+# Create a new random forest classifier for the most important features
+clf_important = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+
+# Train the new classifier on the new dataset containing the most important features
+clf_important.fit(X_important_train, y_train)
+
+
+### Compare The Accuracy Of Our Full Feature Classifier To Our Limited Feature Classifier
+# Apply The Full Featured Classifier To The Test Data
+y_pred = clf.predict(X_test)
+
+# View The Accuracy Of Our Full Feature (4 Features) Model
+accuracy_score(y_test, y_pred)
+
+# Apply The Full Featured Classifier To The Test Data
+y_important_pred = clf_important.predict(X_important_test)
+
+# View The Accuracy Of Our Limited Feature (2 Features) Model
+accuracy_score(y_test, y_important_pred)
